033d8ece3a21d210f612ef6f5d27a1a913cc7040
Blaster <blasterdp@gmail.com>
2022-05-02 16:19:16 -0400

    Ejemplo para modificar el estilo y implementar el modo dia o noche

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Comp1Component } from './modo-oscuro/comp1/comp1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:Comp1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
