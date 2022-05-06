494e89d97ee0b28d19dc23f3e8f5c26ff17bda8c
Blaster <blasterdp@gmail.com>
2022-05-06 13:42:45 -0400

    Eliminado el componente de tarea, modificada la forma de seleccionar la propiedad name.

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaTareasComponent } from './lista-tareas/lista-tareas.component';
-import { TareaComponent } from './tarea/tarea.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -9,7 +8,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ListaTareasComponent,
-    TareaComponent
   ],
   imports: [
     CommonModule,
