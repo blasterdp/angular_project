7864efff2aa7bb41cc5e30c197d9d7d98caeda7a
Blaster <blasterdp@gmail.com>
2022-05-05 21:17:09 -0400

    Incorporada la librería de componentes visuales PrimeNG

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Comp1Component } from './modo-oscuro/comp1/comp1.component';
 
 const routes: Routes = [
-  {path: '', component:Comp1Component}
+  //{path: '', component:Comp1Component}
 ];
 
 @NgModule({
