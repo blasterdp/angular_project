40bbe9e747096b35c0eb3a6b2932ec8f06a92af9
Blaster <blasterdp@gmail.com>
2022-06-22 13:08:24 -0400

    Agregado un grafico linial de prueba y un selector con primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { EChartsOption } from 'echarts';
+
+interface City {
+  name: string,
+  code: string
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular_project-main';
+
+  chartOption: EChartsOption;
+
+  cities: City[];
+
+  selectedCity: City;
+
+  constructor(){
+    this.chartOption = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };
+    this.cities = [
+      {name: 'New York', code: 'NY'},
+      {name: 'Rome',     code: 'RM'},
+      {name: 'London',   code: 'LDN'},
+      {name: 'Istanbul', code: 'IST'},
+      {name: 'Paris',    code: 'PRS'}
+  ];
+  this.selectedCity = this.cities[0]
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import { TitleComponent, TooltipComponent, GridComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer]);
+
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxEchartsModule.forRoot({ echarts }),
+    DropdownModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
