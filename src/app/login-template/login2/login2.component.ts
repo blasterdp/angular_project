94c8e3e01303d07f89d31333c286a07ef967b3cf
Blaster <blasterdp@gmail.com>
2022-06-22 09:38:14 -0400

    Agregado otro formulario de login con otra forma de formulario y un servicio para estudio.

--- src/app/login-template/login2/login2.component.ts
+++ src/app/login-template/login2/login2.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login2',
   templateUrl: './login2.component.html',
-  styleUrls: ['./login2.component.css']
+  styleUrls: ['./login2.component.css'],
+  
 })
 export class Login2Component implements OnInit {
 
